f2580ef612305c8de2c295309bb5907f1619e948
rivalnofirm <rivalnof2@gmail.com>
2021-01-22 02:28:51 +0700

    Adding Heroku required files for running.

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from resources.store import Store, StoreList, NewStore
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/review'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
 app.config["JWT_BLACKLIST_ENABLED"] = True
